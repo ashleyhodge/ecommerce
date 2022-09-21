0f67a6845b8cf126522b0001a96319234eb97341
ashleyhodge <ashhodge@comcast.net>
2022-09-21 17:50:27 -0400

    make store page and product detail

---
+++ utils/data.js
@@ -0,0 +1,78 @@
+const data = {
+  products: [
+    {
+      name: "Free Shirt",
+      slug: 'free-shirt',
+      category: 'Shirts',
+      image: '/images/shirt1.jpeg',
+      price: 70,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'A popular shirt'
+    },
+    {
+      name: "Free Shirt",
+      slug: 'free-shirt6',
+      category: 'Shirts',
+      image: '/images/shirt2.jpeg',
+      price: 80,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'A popular shirt'
+    },
+    {
+      name: "Free Shirt",
+      slug: 'free-shirt5',
+      category: 'Shirts',
+      image: '/images/shirt3.jpeg',
+      price: 90,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'A popular shirt'
+    },
+    {
+      name: "Free Shirt",
+      slug: 'free-shirt2',
+      category: 'Shirts',
+      image: '/images/pants1.jpeg',
+      price: 100,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'A popular shirt'
+    },
+    {
+      name: "Free Shirt",
+      slug: 'free-shirt3',
+      category: 'Shirts',
+      image: '/images/pants2.jpeg',
+      price: 110,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'A popular shirt'
+    },
+    {
+      name: "Free Shirt",
+      slug: 'free-shirt4',
+      category: 'Shirts',
+      image: '/images/pants3.jpeg',
+      price: 120,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'A popular shirt'
+    }
+  ]
+}
+
+export default data;
